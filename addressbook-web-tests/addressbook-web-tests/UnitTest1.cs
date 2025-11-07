5c27d7317cf02633fc130efb4f5bc8e4a7853525
Pavel <natitshow@gmail.com>
2025-11-08 00:59:38 +0300

    created project and added first test

---
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_web_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2 + 3, 4 + 1);
+        }
+    }
+}
