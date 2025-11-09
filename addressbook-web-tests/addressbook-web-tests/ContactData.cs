73d5f23b769e2e3369f96f88797595a61f75ca15
Pavel <natitshow@gmail.com>
2025-11-09 13:51:18 +0300

    added task4: contact creation tests

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,80 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    internal class ContactData
+    {
+        private string firstname;
+        private string middlename;
+        private string lastname;
+        private string nickname;
+        private DateInfo dateInfo;
+
+        internal class DateInfo
+        {
+            private string day;
+            private string month;
+            private string year;
+
+            public DateInfo(string day, string month, string year)
+            {
+                this.day = day;
+                this.month = month;
+                this.year = year;
+            }
+            public string Day { get => day; set => day = value; }
+            public string Month { get => month; set => month = value; }
+            public string Year { get => year; set => year = value; }
+        }
+
+        public ContactData(string firstname, string middlename, string lastname, string nickname, DateInfo dateInfo)
+        {
+            this.firstname = firstname;
+            this.middlename = middlename;
+            this.lastname = lastname;
+            this.nickname = nickname;
+            this.dateInfo = new DateInfo(dateInfo.Day, dateInfo.Month, dateInfo.Year);
+        }
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { this.firstname = value; }
+        }
+
+        public string Middlename
+        {
+            get => middlename;
+            set { this.middlename = value; }
+        }
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set { this.lastname = value; }
+        }
+
+        public string Nickname
+        {
+            get { return nickname; }
+            set { this.nickname = value; }
+        }
+
+        public DateInfo DateInfoProperty
+        {
+            get { return dateInfo; }
+            set { this.dateInfo = value; }
+        }
+    }
+
+}
+
