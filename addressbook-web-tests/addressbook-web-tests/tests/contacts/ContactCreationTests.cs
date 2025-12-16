ede4d5489648e996578301161b5135e72a00e03e
Pavel <natitshow@gmail.com>
2025-12-16 07:53:15 +0300

    hw15: uploading test data from a file

--- addressbook-web-tests/addressbook-web-tests/tests/contacts/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/contacts/ContactCreationTests.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using addressbook_web_tests;
+﻿using addressbook_web_tests;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml.Serialization;
 using WebAddressBookTests.tests;
 
 namespace WebAddressBookTests
@@ -36,8 +39,21 @@ namespace WebAddressBookTests
             return contacts;
         }
 
-     
-        [Test, TestCaseSource("RandomDataContactDataProvider")]
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+            return (List<ContactData>)
+                new XmlSerializer(typeof(List<ContactData>))
+                .Deserialize(new StreamReader(@"TestDataContactXml.xml"));
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(
+                  File.ReadAllText(@"TestDataContactJson.json"));
+        }
+
+
+        [Test, TestCaseSource("ContactDataFromJsonFile")]
         public void ContactCreationTest(ContactData contactData)
         {
             List<ContactData> oldContacts = app.Contacts.GetAllContacts();
